3c8497a151cc2661f5e57bb6ce38efcb5e36ceb0
SaklenManjire <saklenmanjire032@gmail.com>
2026-04-29 08:05:52 +0530

    Experiments added

---
+++ LabWork/Experiment8/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Experiment8.Models
+{
+    public class Customer
+    {
+        public string customerId { get; set; }
+        public string customer_name { get; set; }
+        public string email { get; set; }  
+
+        public string phone_number { get; set; }
+    }
+}
